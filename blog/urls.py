918b12617c6ad8c350d370a4a634105dd73005c9
Dmitry Warkentin <web.warkentin@gmail.com>
2020-02-23 16:36:24 +0400

    Added robots.txt and stuff for heroku

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from blog.sitemaps import PostSitemap
 
@@ -24,6 +25,7 @@ urlpatterns = [
     path('tags/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url'),
     path('tags/<str:slug>/delete/', TagDelete.as_view(), name='tag_delete_url'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name="blog/robots.txt", content_type='text/plain')),
     path('rss', LatestPostsFeed(), name='post_feed'),
     path('api/v1/posts/', include('blog.api.posts.urls'), name='blog-posts-api'),
     path('api/v1/comments/', include('blog.api.comments.urls'), name='blog-comments-api')
